bc40f523f11833d19e0c7cc7e708c54159afce8d
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2020-11-29 14:33:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,55 +17,98 @@ def index():
     return """<!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
             .bg{
-                background-image: url("https://qphs.fs.quoracdn.net/main-qimg-344fafc838d1be7011f7b926f4583cde");
+                /*background-image: url("C:/Users/pc/Desktop/bg3.png");*/
+                background-color: rgb(2, 2, 0);
             }
 
             .basic-text{
                 text-align: center;
-                color: rgb(220, 250, 248);
-                font-size: 18px;
+                color: rgb(217, 233, 236);
+                font-size: 20px;
             }
 
             h1{
                 text-align: center;
-                color: rgb(49, 196, 188);
+                color: rgb(16, 223, 238);
                 text-decoration: underline;
             }
 
             h2{
                 text-align: center;
-                color: rgb(131, 235, 221);
+                color: rgb(40, 236, 194);
             }
 
             a{
                 color: rgb(184, 245, 240);
             }
 
-            h3{
+            h3, b{
                 text-align: center;
-                color: rgb(164, 238, 228);
+                color: rgb(133, 245, 230);
             }
             ul{
                 text-align: center; 
             }
+            .margn div{
+                margin: 70px auto;
+            }
+
+            .onhover a:hover{
+                background-color: rgb(137, 144, 145);
+            }
+
+            h4{
+                color: rgb(139, 99, 248);
+            }
+
+            
 
         </style>
     </head>
     <body class=bg>
-        <h1>PyWhatKit</h1>
-        <h2>Introduction</h2>
         <div class=basic-text>
-            <p><a href="https://pypi.org/project/pywhatkit/" target="blank">PyWhatKit</a> is a Python library with various helpful features solving many daliy life problems. It is an easy to use library which does not requires you to do some additional setup. Currently it has about 100k+ downloads and counting and we regularly update and fix any bug. The source code of this library can be found on <a href="https://github.com/Ankit404butfound/PyatKit">GitHub</a>, you are free update/add some features to it and open a Pull Request, we will review it and include that feature in the next update if the feature was found to be relavent and helpful.</p>
-            <h2>Installation</h2>
-            <p>This library can be installed by the pip command, open you command prompt and type in the following command...</p>
-            <code>pip install pywhatkit</code>
-            <h2>Functions of this library</h2>
-            <h3><b>sendwhatmsg()</b></h3>
-            
+            <h1>PyWhatKit</h1>
+            <div class="onhover">
+                <a href="https://github.com/Ankit404butfound/PywhatKit" target="blank"><img src="https://raw.githubusercontent.com/Ankit404butfound/PyWhatKit/master/Images/github_i.png" style="width:30px;height:30px;"></a>
+                <a href="https://www.quora.com/What-was-one-of-the-first-things-you-made-with-Python/answer/Ankit-Raj-Mahapatra-3" target="blank"><img src="https://raw.githubusercontent.com/Ankit404butfound/PyWhatKit/master/Images/quora.png" style="width:30px;height:30px;"></a>
+                <img src="https://raw.githubusercontent.com/Ankit404butfound/PyWhatKit/master/Images/yt.png" style="width:30px;height:30px;">
+            </div>
+            <div class=margn>
+                <br><a href="https://pypi.org/project/pywhatkit/" target="blank">PyWhatKit</a> is a Python library with various
+                 helpful features. It is an easy to use library which does not requires you to do some additional setup. Currently it has about 100k+ downloads and counting and we regularly update and fix any bug. The source code of this library can be found on <a href="https://github.com/Ankit404butfound/PywhatKit">GitHub</a>, you are free update/add some features to it and open a Pull Request, we will review it and include that feature in the next update if the feature was found to be relavent and helpful.</p>
+                <div>
+                    <h2>Installation</h2>
+                    <p>This library can be installed by the pip command, open you command prompt and type in the following command...</p>
+                    <code>pip install pywhatkit</code>
+                </div>
+                <div>
+                    <h2>Functions of this library</h2>
+                    <p>First import the library using the command <code>import pywhatkit as kit</code> and then proceed to call the functions</p>
+                    <h3>kit.sendwhatmsg()</h3>
+                    <p>
+                        This function can be used to send WhatsApp message at certain time<br><br>
+                        <img src="https://raw.githubusercontent.com/Ankit404butfound/PyWhatKit/master/Images/sendwhatmsg.png" style="width: 900px;height: 500px;"><br>
+                        <h4>The parameters are</h4>
+                        <b>phone_num</b> (required) - Phone number of target with country code
+                        <br><b>message</b> (required) - Message that you want to sendwhatmsg
+                        <br><b>time_hour</b> (required) - Hours at which you want to send message in 24 hour format
+                        <br><b>time_min</b> (required) - Minutes at which you want to send message
+                        <br><b>wait_time</b> (optional, val=20) - Seconds after which the message will be sent after opening the web
+                        <br><b>print_waitTime</b> (optional, val=True) - Will print the remaining time if set to true
+                    </p><br>
+                    <h3>kit.playonyt()</h3>
+                    <p>
+                        This function can be used to search and play a particular video on YouTube by using just the keyword, like "Shape of You song"<br><br>
+                        <img src="https://raw.githubusercontent.com/Ankit404butfound/PyWhatKit/master/Images/playonyt.png" style="width: 900px;height: 500px;"><br>
+                        <h4>The parameters are</h4>
+                        <b>topic</b> (required) - Topic or title that is related to the video 
+                    </p>
+                </div>
+            </div>
         </div>
-
     </body>
 </html>"""
 
