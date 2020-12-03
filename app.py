2582d0494d4a10c338cac4815d927e3b649ad9e3
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2020-12-03 13:40:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,116 @@
-from flask import Flask,abort,render_template,request,redirect,url_for,send_file
-import os
-import random
+from flask import Flask, request, redirect
+import re
+import urllib.request
 import string
+import os
+import numpy as np
+from PIL import Image
+import cv2
+import random
 
 char = string.ascii_lowercase
 file_code_name = {}
 
-app = Flask(__name__)
 UPLOAD_FOLDER = ''
+
+width = 50
+height = 0
+newwidth = 0
+arr = string.ascii_letters
+arr = arr + string.digits + "+,.-? "
+letss = string.ascii_letters
+
+app = Flask(__name__, static_url_path='')
+
+
+def getimg(case,col):
+    global width,height,back
+    try:
+        url = "https://raw.githubusercontent.com/Ankit404butfound/HomeworkMachine/master/Image/%s.png"%case
+        imglink=urllib.request.urlopen(url)
+    except:
+        url = "https://raw.githubusercontent.com/Ankit404butfound/HomeworkMachine/master/Image/%s.PNG"%case
+        imglink=urllib.request.urlopen(url)
+    imgNp=np.array(bytearray(imglink.read()))
+    img = cv2.imdecode(imgNp,-1)
+    cv2.imwrite(r"%s.png"%case,img)
+    img = cv2.imread("%s.png"%case)
+    img[np.where((img!=[255,255,255]).all(axis=2))] = col
+    cv2.imwrite("chr.png",img)
+    cases = Image.open("chr.png")
+    back.paste(cases,(width,height))
+    newwidth = cases.width
+    width = width + newwidth
+
+
+def text_to_handwriting(string,rgb=[0,0,138]):
+    """Convert the texts passed into handwritten characters"""
+    save_path = ""
+    for i in range(10):
+        save_path = save_path+random.choice(letss)
+    try:
+        global arr, width, height, back
+        #rgb.reverse() not working, IDK why.
+        back = Image.open("zback.png")
+        rgb = [rgb[2],rgb[1],rgb[0]]
+        count = -1
+        lst = string.split()
+        for letter in string:
+            if width + 150 >= back.width or ord(letter) == 10:
+                height = height + 227
+                width = 50
+            if letter in arr:
+                if letter == " ":
+                    count += 1
+                    letter = "zspace"
+                    wrdlen = len(lst[count+1])
+                    if wrdlen*110 >= back.width-width:
+                        width = 50
+                        height = height+227
+                    
+                elif letter.isupper():
+                    letter = "c"+letter.lower()
+                elif letter == ",":
+                    letter = "coma"
+                elif letter == ".":
+                    letter = "fs"
+                elif letter == "?":
+                    letter = "que"
+                    
+                getimg(letter,rgb)
+                
+        #back.show()
+        back.save(f"static/{save_path}.png")
+        back.close()
+        back = Image.open("zback.png")
+        #rgb = [0,0,138]
+        width = 50
+        height = 0
+        newwidth = 0
+        return save_path
+    
+    except Exception as e:
+        try:
+            import cv2
+            global cv2
+            print(e)
+            url = "https://raw.githubusercontent.com/Ankit404butfound/HomeworkMachine/master/Image/zback.png"
+            imglink=urllib.request.urlopen(url)
+            imgNp=np.array(bytearray(imglink.read()))
+            img = cv2.imdecode(imgNp,-1)
+            cv2.imwrite("zback.png",img)
+        except Exception as e:
+            print(e)
+
+
+
+@app.route("/handwriting")
+def api():
+    text = request.args.get("text")
+    path = text_to_handwriting(str(text))
+    return redirect(f"https://pywhatkit.herokuapp.com/{path}.png",302)
+
+
 
 @app.route("/")
 def new():
