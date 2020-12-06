e7671b7a9a8869b980c78463c613c03f9aaa6381
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2020-12-06 20:32:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,59 +53,57 @@ def text_to_handwriting(string,rgb=[0,0,138]):
     save_path = ""
     for i in range(10):
         save_path = save_path+random.choice(letss)
-    try:
-        global arr, width, height, back
-        #rgb.reverse() not working, IDK why.
-        back = Image.open("zback.png")
-        rgb = [rgb[2],rgb[1],rgb[0]]
-        count = -1
-        lst = string.split()
-        for letter in string:
-            if width + 150 >= back.width or ord(letter) == 10:
-                height = height + 227
-                width = 50
-            if letter in arr:
-                if letter == " ":
-                    count += 1
-                    letter = "zspace"
-                    wrdlen = len(lst[count+1])
-                    if wrdlen*110 >= back.width-width:
-                        width = 50
-                        height = height+227
-                    
-                elif letter.isupper():
-                    letter = "c"+letter.lower()
-                elif letter == ",":
-                    letter = "coma"
-                elif letter == ".":
-                    letter = "fs"
-                elif letter == "?":
-                    letter = "que"
-                    
-                getimg(letter,rgb)
-                
-        #back.show()
-        back.save(f"static/{save_path}.png")
-        back.close()
-        back = Image.open("zback.png")
-        #rgb = [0,0,138]
-        width = 50
-        height = 0
-        newwidth = 0
-        return save_path
     
-    except Exception as e:
-        try:
-            import cv2
-            global cv2
-            print(e)
-            url = "https://raw.githubusercontent.com/Ankit404butfound/HomeworkMachine/master/Image/zback.png"
-            imglink=urllib.request.urlopen(url)
-            imgNp=np.array(bytearray(imglink.read()))
-            img = cv2.imdecode(imgNp,-1)
-            cv2.imwrite("zback.png",img)
-        except Exception as e:
-            print(e)
+    global arr, width, height, back
+    #rgb.reverse() not working, IDK why.
+    try:
+        back = Image.open("zback.png")
+    except:
+        url = "https://raw.githubusercontent.com/Ankit404butfound/HomeworkMachine/master/Image/zback.png"
+        imglink=urllib.request.urlopen(url)
+        imgNp=np.array(bytearray(imglink.read()))
+        img = cv2.imdecode(imgNp,-1)
+        cv2.imwrite("zback.png",img)
+        back = Image.open("zback.png")
+    rgb = [rgb[2],rgb[1],rgb[0]]
+    count = -1
+    lst = string.split()
+    for letter in string:
+        if width + 150 >= back.width or ord(letter) == 10:
+            height = height + 227
+            width = 50
+        if letter in arr:
+            if letter == " ":
+                count += 1
+                letter = "zspace"
+                wrdlen = len(lst[count+1])
+                if wrdlen*110 >= back.width-width:
+                    width = 50
+                    height = height+227
+
+            elif letter.isupper():
+                letter = "c"+letter.lower()
+            elif letter == ",":
+                letter = "coma"
+            elif letter == ".":
+                letter = "fs"
+            elif letter == "?":
+                letter = "que"
+
+            getimg(letter,rgb)
+
+    #back.show()
+    back.save(f"static/{save_path}.png")
+    back.close()
+    back = Image.open("zback.png")
+    #rgb = [0,0,138]
+    width = 50
+    height = 0
+    newwidth = 0
+    return save_path
+    
+          
+   
 
 
 
